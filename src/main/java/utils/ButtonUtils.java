681b07425a6b641f6cc39e83891c2bd872f3c1b2
ramazancoskun <ramazan1052k@gmail.com>
2025-08-30 17:29:54 +0300

    ButtonUtils ve WaitUtils tamamlandı

--- src/main/java/utils/ButtonUtils.java
+++ src/main/java/utils/ButtonUtils.java
@@ -1,4 +1,69 @@
 package utils;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+
 public class ButtonUtils {
+
+    /**
+     * Verilen WebElement'e tıklar.
+     */
+    public static void clickButton(WebElement element) {
+        element.click();
+    }
+
+    /**
+     * Element görünür olana kadar bekler ve ardından tıklar.
+     */
+    public static void waitUntilVisibleAndClick(WebDriver driver, WebElement element, int timeoutInSeconds) {
+        WaitUtils.waitForElementVisible(driver, element, timeoutInSeconds);
+        element.click();
+    }
+
+    /**
+     * JavaScript kullanarak verilen WebElement'e tıklar.
+     */
+    public static void clickButtonWithJS(WebDriver driver, WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
+
+    /**
+     * Element tıklanabilir olana kadar bekler ve ardından tıklar.
+     */
+    public static void waitUntilClickableAndClick(WebDriver driver, WebElement element, int seconds) {
+        WaitUtils.waitForElementClickable(driver, element, seconds);
+        element.click();
+    }
+
+    /**
+     * WebElement üzerinde mouse hover yapar ve ardından tıklar.
+     */
+    public static void hoverAndClick(WebDriver driver, WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).click().perform();
+    }
+
+    /**
+     * WebElement'in görünür olup olmadığını kontrol eder.
+     */
+    public static boolean isButtonVisible(WebElement element) {
+        return element.isDisplayed();
+    }
+
+    /**
+     * WebElement'in enable durumunu kontrol eder.
+     */
+    public static boolean isButtonEnabled(WebElement element) {
+        return element.isEnabled();
+    }
+
+    /**
+     * WebElement'in metninin beklenen metinle eşleşip eşleşmediğini kontrol eder.
+     */
+    public static boolean doesButtonHaveText(WebElement element, String expectedText) {
+        return element.getText().equals(expectedText);
+    }
 }
--- src/main/java/utils/WaitUtils.java
+++ src/main/java/utils/WaitUtils.java
@@ -1,4 +1,35 @@
 package utils;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WaitUtils {
+
+    public static void waitForElementVisible(WebDriver driver, WebElement element, int seconds) {
+        new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.visibilityOf(element));
+    }
+    public static void waitForElementClickable(WebDriver driver, WebElement element, int seconds) {
+        new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.elementToBeClickable(element));
+    }
+    public static void waitForElementInvisible(WebDriver driver, WebElement element, int seconds) {
+        new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.invisibilityOf(element));
+    }
+    public static void waitForPageLoad(WebDriver driver, int seconds) {
+        new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(webDriver -> ((JavascriptExecutor) webDriver)
+                        .executeScript("return document.readyState").equals("complete"));
+    }
+    public static void waitForAlert(WebDriver driver, int seconds) {
+        new WebDriverWait(driver, Duration.ofSeconds(seconds))
+                .until(ExpectedConditions.alertIsPresent());
+    }
+
 }
